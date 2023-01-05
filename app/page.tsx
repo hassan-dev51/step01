3f0370736a44ae43f8d04b4c854f9f69d891f9a4
hassan-dev51 <hassanmunir363@gmail.com>
2023-01-05 21:17:04 +0500

    second commit

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,9 @@ import React from "react";
 const page = () => {
   return (
     <div className="bg-slate-300 opacity-0.3">
-      <h2 className="text-3xl p-3 text-center text-red-500">Hello World</h2>
+      <h2 className="text-3xl p-3 text-center text-red-500">
+        Hello World 2023
+      </h2>
     </div>
   );
 };
